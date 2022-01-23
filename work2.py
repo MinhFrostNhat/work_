ea959751458fc9159bffceec3c05c846cf2c9ba8
nhatvmgcs18725 <66456618+nhatvmgcs18725@users.noreply.github.com>
2022-01-23 16:26:52 +0700

    rewrite_code

--- work2.py
+++ work2.py
@@ -1,32 +1,30 @@
 import requests
-from work import set_log
+from work import set_loggin
 import sqlite3
 import time
-import numpy as np
-logger = set_log()
+
+logger = set_loggin()
 
 
-def run():
+def api_data():
     try:
         endpoint = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
         body = {
-                "page": 1,
-                "rows": 20,
-                "payTypes": [],
-                "asset": "USDT",
-                "tradeType": "BUY",
-                "fiat": "VND",
-                # "publisherType":null,
-                "merchantCheck": False,
-            }
+            "page": 1,
+            "rows": 20,
+            "payTypes": [],
+            "asset": "USDT",
+            "tradeType": "BUY",
+            "fiat": "VND",
+            # "publisherType":null,
+            "merchantCheck": False,
+        }
         headers = {
-                "Content-Type": "application/json",
-            }
-        logger.info("status 200: ok")
-    except Exception as e:
-        return f"logger.error('error status 400')"
-    try:
-            response = requests.post(url=endpoint, json=body, headers=headers)
+            "Content-Type": "application/jsonz",
+        }
+        response = requests.post(url=endpoint, json=body, headers=headers)
+        if response.status_code == 200:
+            logger.info("status code 200: ok")
             content = response.json()['data']
             with sqlite3.connect('price.db') as conn:
                 curs = conn.cursor()
@@ -34,21 +32,25 @@ def run():
                 logger.info('CREATE TABLE')
                 for each in content:
                     price = float(each['adv']['price'])
-                    print(price)
                     curs.execute("INSERT INTO price VALUES (?)", (price,))
-                    logger.info('INSERT DATA TO DATABASE')
-    except Exception as E:
-            return f"{logger.error('error json shutdown')}"
-    try:
-                conn = sqlite3.connect('price.db')
+                logger.info('INSERT DATA TO DATABASE')
                 cur = conn.cursor()
                 cur.execute("SELECT AVG(price) FROM price")
                 rows = cur.fetchall()
-                for row in rows:
-                    logger.warning(row)
-    except Exception as e:
-        return logger.error('cant not do this error database')
-if __name__== "__main__" :
+                for rw in rows:
+                    for i in rw:
+                        if i > 23600:
+                            logger.warning(f"it's go to upper, price now is: {i}")
+                        else:
+                            logger.info(f"ok price now : {i}")
+        else:
+            logger.error("status code 400: bad request")
+            return api_data()
+    except Exception:
+        logger.error('An error occurred')
+
+
+def repeat_time():
     start_time = time.time()
     seconds = 0
     while True:
@@ -56,4 +58,9 @@ if __name__== "__main__" :
         elapsed_time = current_time - start_time
         if elapsed_time > seconds:
             seconds = seconds + 60
-            run()
+            api_data()
+
+
+if __name__ == "__main__":
+    repeat_time()
+
